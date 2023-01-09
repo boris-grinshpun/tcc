c1c7999e5fe66cda4de67316a2d1a395937cd7d0
Boris <boris.grinshpun@hotmail.com>
2023-01-09 18:38:40 +0200

    second

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,59 @@ import { charachterGetAll, locationGetAll, episodeGetAll } from './api/api'
 
 function App() {
   const [count, setCount] = useState(0)
-  let carachters = []
+  const [charachters, setCharachters] = useState([])
+  const [episodes, setEpisodes] = useState([])
+  const [earthCharachters, setEarthCharachters] = useState([])
+  const [earthAppearances, setEarthAppearances] = useState({}) // {[charachter]: numAppearances}
   let locations = []
-  let episodes = []
+  let earthVal = "Earth (C-137)"
   let data = []
   useEffect(() => {
     async function fechData() {
       try {
-        data = await Promise.all([charachterGetAll(), locationGetAll(), episodeGetAll()])
-        carachters = data[0]
-        locations = data[1]
-        episodes = data[2]
-      } catch (err){
+        data = await Promise.all([charachterGetAll(), episodeGetAll()])
+        setCharachters(data[0])
+        // locations = data[1]
+        setEpisodes(data[1])
+        // calcEarthAppearances()
+        // console.log(earthCharachters)
+        // console.log(charachters,
+        //   episodes)
+      } catch (err) {
         console.log(err)
       }
     }
     fechData()
   }, [])
+  useEffect(() => {
+    // console.log(charachters)
+    const fromEarth = charachters.filter(charachter => charachter?.origin?.name === earthVal)
+    console.log(fromEarth)
+    const initEarthAppearances = fromEarth.reduce((acc,char) => {
+      acc[char.id.toString()] = 0
+      return acc
+    },{})
+    console.log(initEarthAppearances)
+    setEarthAppearances(initEarthAppearances)
+    setEarthCharachters(fromEarth)
+  }, [charachters])
+
+  useEffect(() => {
+    console.log(earthCharachters)
+    let earthAppearances = episodes.reduce(episode => {
+      return
+    }, {})
+  }, [earthCharachters])
+
+  useEffect(()=>{
+    console.log(episodes)
+  },[episodes])
+
+  // function calcEarthAppearances(){
+  //   earthAppearances = episodes.reduce(episode=>{
+  //     return
+  //   },earthAppearances)
+  // }
   return (
     <div className="App">
       <Layout />
--- src/api/api.js
+++ src/api/api.js
@@ -1,37 +1,57 @@
 const CHARACHTER_API_URL = 'https://rickandmortyapi.com/api/character'
 const LOCATION_API_URL = 'https://rickandmortyapi.com/api/location'
-const EPISODE_API_URL = 'https://rickandmortyapi.com/api/location'
-export function locationApi(params = []) {
+const EPISODE_API_URL = 'https://rickandmortyapi.com/api/episode'
+const apiResultsPerPage = 20
 
-}
 export function charachterGetAll() {
     return fetchAll(CHARACHTER_API_URL)
 }
 export function locationGetAll(params = []) {
     return fetchAll(LOCATION_API_URL)
-
 }
 export function episodeGetAll(params = []) {
     return fetchAll(EPISODE_API_URL)
 }
-    function fetchAll(url){
-        return fetch(url)
-            .then(data => data.json())
-            .then(data => {
-                const firstPage = data
-                const pages = data.info.pages
-                const allPages = []
-                for (let page = 2; page <= pages; page++) {
-                    allPages.push(`${url}/?page${page}`)
-                }
-                const promisses = allPages.map(page => {
-                    return fetch(page)
+function fetchAll(url) {
+    return fetch(url)
+        .then(data => data.json())
+        .then(data => {
+            const firstPage = data
+            const totalPages = data.info.pages
+            const ids = (totalPages - 1) * apiResultsPerPage
+            const idList = []
+            const from = apiResultsPerPage + 1
+            for (let id = from; id <= ids; id++) {
+                idList.push(id)
+            }
+            const fetchByIds = fetch(`${url}/${idList.join(",")}`)
+            const lastPage = fetch(`${url}/?page=${totalPages}`)
+            return Promise.all([fetchByIds, lastPage])
+                .then(responses => {
+                    return Promise.all(responses.map(res => res.json()))
+                        .then(data => [firstPage.results, ...data[0], data[1]])
                 })
-                return Promise.all(promisses)
-                    .then(responses =>
-                        Promise.all(responses.map(res => res.json()))
-                        .then(data=>[firstPage, ...data])
-                    )
-            })
-    
-    }
+        })
+
+}
+// function fetchAll(url) {
+//     return fetch(url)
+//         .then(data => data.json())
+//         .then(data => {
+//             const firstPage = data
+//             const pages = data.info.pages
+//             const allPages = []
+//             for (let page = 2; page <= pages; page++) {
+//                 allPages.push(`${url}/?page=${page}`)
+//             }
+//             const promisses = allPages.map(page => {
+//                 return fetch(page)
+//             })
+//             return Promise.all(promisses)
+//                 .then(responses =>
+//                     Promise.all(responses.map(res => res.json()))
+//                         .then(data => [firstPage, ...data])
+//                 )
+//         })
+
+// }
